8bc4cad0cd9adca4c6efd7586cc3ddecf79c2d59
nrg_9000 <nrg.devcode@gmail.com>
2024-08-05 02:33:12 +0530

    Started Game Class

--- project.py
+++
@@ -1,5 +0,0 @@
-"""
-This is the first attempt to code the milestone project 2 of the python course
-This will hold all of the necessary comments
-This project should include comments in every step of the way
-"""
